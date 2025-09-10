b02e8c21c694e5778cc93cb554247af5cdbf43e8
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2025-09-10 12:14:22 -0300

    compare ResponseHeadersPolicies & RequestPolicies

--- src/logic/comparePolicies.ts
+++ src/logic/comparePolicies.ts
@@ -1,6 +1,10 @@
 import {
   CachePolicyList,
   CachePolicySummary,
+  OriginRequestPolicyList,
+  OriginRequestPolicySummary,
+  ResponseHeadersPolicyList,
+  ResponseHeadersPolicySummary,
 } from "@aws-sdk/client-cloudfront";
 
 export const compareCachePoliciesByName = (
@@ -22,6 +26,54 @@ export const compareCachePoliciesByName = (
   // Devuelve los IDs, filtrando los falsy
   const missingIds: string[] = missing
     .map(policy => policy.CachePolicy?.Id)
+    .filter((id): id is string => !!id);
+
+  return missingIds;
+};
+
+export const compareResponseHeadersPoliciesByName = (
+  originPolicies: ResponseHeadersPolicyList,
+  destinationPolicies: ResponseHeadersPolicyList,
+): string[] => {
+  // Solo incluimos nombres válidos
+  const destinationNames: string[] = destinationPolicies.Items
+    ?.map(item => item.ResponseHeadersPolicy?.ResponseHeadersPolicyConfig?.Name)
+    .filter((name): name is string => !!name) || [];
+
+  const destinationSet = new Set(destinationNames);
+  const missing: ResponseHeadersPolicySummary[] = originPolicies.Items
+    ?.filter(policy => {
+      const name = policy.ResponseHeadersPolicy?.ResponseHeadersPolicyConfig?.Name;
+      return typeof name === 'string' && !destinationSet.has(name);
+    }) || [];
+
+  // Devuelve los IDs, filtrando los falsy
+  const missingIds: string[] = missing
+    .map(policy => policy.ResponseHeadersPolicy?.Id)
+    .filter((id): id is string => !!id);
+
+  return missingIds;
+};
+
+export const compareOriginRequestPoliciesByName = (
+  originPolicies: OriginRequestPolicyList,
+  destinationPolicies: OriginRequestPolicyList,
+): string[] => {
+  // Solo incluimos nombres válidos
+  const destinationNames: string[] = destinationPolicies.Items
+    ?.map(item => item.OriginRequestPolicy?.OriginRequestPolicyConfig?.Name)
+    .filter((name): name is string => !!name) || [];
+
+  const destinationSet = new Set(destinationNames);
+  const missing: OriginRequestPolicySummary[] = originPolicies.Items
+    ?.filter(policy => {
+      const name = policy.OriginRequestPolicy?.OriginRequestPolicyConfig?.Name;
+      return typeof name === 'string' && !destinationSet.has(name);
+    }) || [];
+
+  // Devuelve los IDs, filtrando los falsy
+  const missingIds: string[] = missing
+    .map(policy => policy.OriginRequestPolicy?.Id)
     .filter((id): id is string => !!id);
 
   return missingIds;
