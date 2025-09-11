9df0a068f5b8b7a357ad1930514d876533da313e
Lorenzo Caldera <115328112+LorenzoCaldera@users.noreply.github.com>
2025-09-11 11:15:27 -0300

    [rf] extract policies ids for reusability
    Extracted a generic extractPolicyIds function to reduce code duplication across cache, response headers, and origin request policy comparisons. Updated the specific comparison functions to use the new generic implementation.

--- src/logic/extractPoliciesIds.ts
+++ src/logic/extractPoliciesIds.ts
@@ -1,70 +1,62 @@
-import type { DistributionConfig } from "@aws-sdk/client-cloudfront";
+import type {
+  CacheBehavior,
+  DefaultCacheBehavior,
+  DistributionConfig
+} from "@aws-sdk/client-cloudfront";
+
+/**
+ * Extrae los IDs de las políticas de caché, solicitud de origen y respuesta de un objeto de configuración de distribución de CloudFront.
+ * @param config Objeto de configuración de distribución de CloudFront.
+ * @param getId Función para obtener el ID de la política.
+ * @returns Un conjunto de IDs de políticas.
+ */
+const extractPolicyIds = (
+  config: DistributionConfig,
+  getId: (policy?: DefaultCacheBehavior | CacheBehavior) => string | undefined
+): Set<string> => {
+  const ids = new Set<string>();
+
+  const defaultId = getId(config.DefaultCacheBehavior);
+  if (defaultId) ids.add(defaultId);
+
+  const behaviorsId = config.CacheBehaviors?.Items?.map(getId).filter((id): id is string => !!id) ?? [];
+  if (behaviorsId.length > 0) behaviorsId.forEach(id => ids.add(id))
+
+  return ids;
+}
 
 /**
  * Extrae todas las CachePolicyId de DefaultCacheBehavior y CacheBehaviors.
  * @param config Objeto de configuración de distribución de CloudFront.
  * @returns Set de IDs de políticas de caché.
  */
-export function extractCachePolicyIds(config: DistributionConfig): Set<string> {
-  const ids = new Set<string>(); // Usamos Set para evitar duplicados
-
-  if (config.DefaultCacheBehavior?.CachePolicyId) {
-    ids.add(config.DefaultCacheBehavior.CachePolicyId);
-  }
-
-  if (config.CacheBehaviors?.Items) {
-    for (const behavior of config.CacheBehaviors.Items) {
-      if (behavior.CachePolicyId) {
-        ids.add(behavior.CachePolicyId);
-      }
-    }
-  }
-
-  return ids;
-}
+export const extractCachePolicyIds = (config: DistributionConfig): Set<string> => (
+  extractPolicyIds(
+    config,
+    (policy?: DefaultCacheBehavior | CacheBehavior) => policy?.CachePolicyId,
+  )
+)
 
 /**
  * Extrae todas las OriginRequestPolicyId de DefaultCacheBehavior y CacheBehaviors.
  * @param config Objeto de configuración de distribución de CloudFront.
  * @returns Set de IDs de políticas de solicitud de origen.
  */
-export function extractOriginRequestPolicyIds(config: DistributionConfig): Set<string> {
-  const ids = new Set<string>(); // Usamos Set para evitar duplicados
-
-  if (config.DefaultCacheBehavior?.OriginRequestPolicyId) {
-    ids.add(config.DefaultCacheBehavior.OriginRequestPolicyId);
-  }
-
-  if (config.CacheBehaviors?.Items) {
-    for (const behavior of config.CacheBehaviors.Items) {
-      if (behavior.OriginRequestPolicyId) {
-        ids.add(behavior.OriginRequestPolicyId);
-      }
-    }
-  }
-
-  return ids;
-}
+export const extractOriginRequestPolicyIds = (config: DistributionConfig): Set<string> => (
+  extractPolicyIds(
+    config,
+    (policy?: DefaultCacheBehavior | CacheBehavior) => policy?.OriginRequestPolicyId,
+  )
+)
 
 /**
  * Extrae todas las ResponseHeadersPolicyId de DefaultCacheBehavior y CacheBehaviors.
  * @param config Objeto de configuración de distribución de CloudFront.
  * @returns Set de IDs de políticas de respuesta.
  */
-export function extractResponseHeadersPolicyIds(config: DistributionConfig): Set<string> {
-  const ids = new Set<string>(); // Usamos Set para evitar duplicados
-
-  if (config.DefaultCacheBehavior?.ResponseHeadersPolicyId) {
-    ids.add(config.DefaultCacheBehavior.ResponseHeadersPolicyId);
-  }
-
-  if (config.CacheBehaviors?.Items) {
-    for (const behavior of config.CacheBehaviors.Items) {
-      if (behavior.ResponseHeadersPolicyId) {
-        ids.add(behavior.ResponseHeadersPolicyId);
-      }
-    }
-  }
-
-  return ids;
-}
+export const extractResponseHeadersPolicyIds = (config: DistributionConfig): Set<string> => (
+  extractPolicyIds(
+    config,
+    (policy?: DefaultCacheBehavior | CacheBehavior) => policy?.ResponseHeadersPolicyId,
+  )
+)
